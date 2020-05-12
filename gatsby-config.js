6ebf09beba805e24e30a97af976c7177ef0deb84
Tomas <haverlatom7@gmail.com>
2020-05-12 22:58:57 +0200

    initial push

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,9 @@ module.exports = {
     author: `@gatsbyjs`,
   },
   plugins: [
+ 
+     `gatsby-plugin-zopfli`,
+    
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
